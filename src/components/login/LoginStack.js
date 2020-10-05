2cb21ccbf91520602fc7749350636f33d4e4351e
Alfredo ws <sick_freedylp@hotmail.com>
2020-10-05 14:15:28 -0500

    adding-login-facebook

--- src/components/login/LoginStack.js
+++ src/components/login/LoginStack.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
 import Welcome from './Welcome'
-import Login from './Login'
+import Login from '../Facebook'
 
 
 const Stack = createStackNavigator()
